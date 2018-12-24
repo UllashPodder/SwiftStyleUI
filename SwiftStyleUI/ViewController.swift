6d1ae30de1f3e4fc6a36cf0223a35a2c46d7ed60
Ullash Podder <ullash.podder@gmail.com>
2018-12-24 11:51:29 +0600

    Initial Commit

---
+++ SwiftStyleUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStyleUI
+//
+//  Created by Ullash Podder on 24/12/18.
+//  Copyright © 2018 Ullash Podder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
